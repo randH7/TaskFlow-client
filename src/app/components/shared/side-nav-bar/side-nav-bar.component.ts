f7a7f80d4947992ee426b99403ff363f9ccb3f4d
randH7 <rand_alharthi@hotmail.com>
2023-11-21 22:29:57 +0300

    implemnt logout and change the design of sidebar

--- src/app/components/shared/side-nav-bar/side-nav-bar.component.ts
+++ src/app/components/shared/side-nav-bar/side-nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { navbarData } from './nav-data';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { Router } from '@angular/router';
 
 declare function sideBarAction(elementId: string, elementId2: string): void;
 
@@ -9,6 +11,14 @@ declare function sideBarAction(elementId: string, elementId2: string): void;
   styleUrls: ['./side-nav-bar.component.css']
 })
 export class SideNavBarComponent {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    console.log("logging out");
+    this.router.navigate(["/login"]);
+  }
 
   sideBarAction(): void {
     console.log(11)
